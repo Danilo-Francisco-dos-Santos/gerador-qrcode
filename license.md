3dcf3c776ad23b6b5858d41ee703a88fb5f7d552
Danilo Santos <danilohhe@gmail.com>
2025-01-16 09:07:23 -0300

    Alterações para o geração do instalador

---
+++ license.md
@@ -0,0 +1,7 @@
+Direitos autorais © 2025 Danilo Santos 
+
+E concedida permissao, gratuitamente, a qualquer pessoa que obtenha uma copia deste software e dos arquivos de documentacao associados (o "Software"), para lidar com o Software sem restricoes, incluindo, sem limitacao, os direitos de usar, copiar, modificar, mesclar, publicar, distribuir, sublicenciar e/ou vender copias do Software, e permitir que as pessoas a quem o Software e fornecido facam isso, sujeito as seguintes condicoes:
+
+O aviso de direitos autorais acima e este aviso de permissao devem ser incluidos em todos copias ou partes substanciais do Software.
+
+O SOFTWARE E FORNECIDO "COMO ESTA", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU IMPLICITA, INCLUINDO, MAS NAO SE LIMITANDO AS GARANTIAS DE COMERCIALIZACAO, ADEQUACAO A UMA FINALIDADE ESPECAFICA E NAO VIOLACAO. EM NENHUM CASO, OS AUTORES OU TITULARES DOS DIREITOS AUTORAIS SERAO RESPONSAVEIS POR QUALQUER REIVINDICACAO, DANOS OU OUTRA RESPONSABILIDADE, SEJA EM UMA ACAO DE CONTRATO, DELITO OU OUTRA FORMA, DECORRENTE DE, FORA OU EM CONEXAO COM O SOFTWARE OU O USO OU OUTRAS NEGOCIACOES NO SOFTWARE.
--- main.js
+++ main.js
@@ -1,15 +1,77 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
+/*Configuração da opção sobre*/
+const { dialog } = require('electron');
+const { version } = require('node:os');
+const { versions } = require('node:process');
+const path = require('node:path');
+
+/*Verifica se tem atualização e notifica o usuário*/
+const { autoUpdater } = require("electron-updater");
+autoUpdater.checkForUpdatesAndNotify();
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 600,
+        height: 630,
+        /***Adicionando o logo***/
+        icon: __dirname + "/image/icone.png",
+        resizable: false,
     })
 
     win.loadFile('index.html')
 }
 
+/***Template de Menu***/
+const templateMenu = [
+    {
+        label: 'Menu',
+        submenu: [
+            {
+                label: 'Fechar',
+                role: process.platform === 'darwin' ? 'close' : 'quit'
+            },
+
+            {
+                label: 'Atualizar',
+                role: 'reload'
+            },
+
+            {
+                label: 'Sobre',
+                click: () => {
+                    dialog.showMessageBox({
+                        type: 'info',
+                        title: 'Sobre',
+                        message: 'Versão: ' + '1.0.0' + '\n'
+                            + 'Electron: ' + versions.electron + '\n'
+                            + 'Node.js: ' + versions.node + '\n'
+                            + 'Chromium: ' + versions.chrome + '\n'
+                            + 'SO: ' + version()
+                    })
+                }
+            }
+        ]
+    }
+];
+
+/***Menu***/
+const menu = Menu.buildFromTemplate(templateMenu);
+Menu.setApplicationMenu(menu);
+
 app.whenReady().then(() => {
     createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
